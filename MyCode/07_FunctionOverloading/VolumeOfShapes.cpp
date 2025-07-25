4f1fd83f21c15738aa693e72b1a094a7be880186
Atharv-programming <atharvambekar2509@gmail.com>
2025-07-25 12:18:04 +0530

    Function Overloading

---
+++ MyCode/07_FunctionOverloading/VolumeOfShapes.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int volume(int side);
+float volume(float length, float width, float breadth);
+float volume(float radius);
+
+int main(void)
+{
+    std::cout<<"Volume of Cube "<<volume(4)<<std::endl;
+    std::cout<<"Volume of CUboid "<<volume(3.0f, 4.0f, 5.0f)<<std::endl;
+    std::cout<<"volume of a sphere "<<volume(4.0f)<<std::endl;
+
+    return(0);
+}
+
+int volume(int side)
+{
+    return(side * side * side);
+}
+
+float volume(float length, float width, float breadth)
+{
+    return(length*width*breadth);
+}
+
+float volume(float radius)
+{
+    return((4/3)* 3.14f * (radius*radius*radius));
+}
+
