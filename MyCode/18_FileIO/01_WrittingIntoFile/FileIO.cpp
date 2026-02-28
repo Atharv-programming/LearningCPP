a06dd7434c628061bc68b17ca2d7dd246ffffbeb
Atharv-programming <atharvambekar2509@gmail.com>
2026-03-01 01:00:08 +0530

    File I/O

---
+++ MyCode/18_FileIO/01_WrittingIntoFile/FileIO.cpp
@@ -0,0 +1,21 @@
+/*
+    Author : Atharv Anil Ambekar
+    Date : 28th March 2026
+    Purpose : learning about File I/O (Writting into File)
+*/
+#include<iostream>
+#include<fstream>
+
+int main(void)
+{
+    std::ofstream out;
+    out.open("abc.txt");
+
+    std::string st = "Atharva Was Here...";
+
+    out<<st;
+
+    out.close();
+
+    return(0);
+}
---
+++ MyCode/18_FileIO/02_ReadingFromFile/FileIO.cpp
@@ -0,0 +1,29 @@
+/*
+    Author : Atharv Anil Ambekar
+    Date : 28th March 2026
+    Purpose : learning about File I/O (Writting into File)
+*/
+#include<iostream>
+#include<fstream>
+#include<string>
+
+int main(void)
+{
+    std::ifstream in;
+    in.open("abc.txt");
+
+    std::string st1;
+    std::string st2;
+
+    in>>st1;
+    std::cout<<"using '>>' : "<<st1<<std::endl;
+
+    in.seekg(0); // Moves the file pointer again to the first place
+
+    getline(in, st2);
+    std::cout<<"using 'getline()' : "<<st2<<std::endl;
+
+    in.close();
+
+    return(0);
+}
